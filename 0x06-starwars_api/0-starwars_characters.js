7196a03059ef9182c6a3c76cf40f3911f41ea2c9
eMode18 <ewagandi@gmail.com>
2024-07-12 06:37:36 +0000

    task correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,26 @@
 #!/usr/bin/node
+
 const request = require('request');
-const SWAPI_URL = 'https://swapi-api.hbtn.io/api'; // API URL for Star Wars data
+const SWAPI_URL = 'https://swapi-api.hbtn.io/api'; // Renamed API_URL to SWAPI_URL
 
 if (process.argv.length > 2) {
-  // Fetch film data based on the provided film ID
   request(`${SWAPI_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
-      console.error('Error fetching film data:', err);
-      return;
+      console.log(err);
     }
-
-    const charactersURLs = JSON.parse(body).characters; // URLs of characters in the film
-    const characterNamesPromises = charactersURLs.map(
+    const charactersURL = JSON.parse(body).characters;
+    const characterNames = charactersURL.map(
       url => new Promise((resolve, reject) => {
-        // Fetch character data for each URL
         request(url, (promiseErr, __, charactersReqBody) => {
           if (promiseErr) {
             reject(promiseErr);
           }
-          resolve(JSON.parse(charactersReqBody).name); // Extract character names
+          resolve(JSON.parse(charactersReqBody).name);
         });
-      })
-    );
+      }));
 
-    // Resolve all promises and log character names
-    Promise.all(characterNamesPromises)
-      .then(names => console.log('Character names:\n', names.join('\n')))
-      .catch(allErr => console.error('Error fetching character names:', allErr));
+    Promise.all(characterNames)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
-
