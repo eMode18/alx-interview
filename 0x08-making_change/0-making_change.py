8fbb9746f02325d667df2d3238d16259b0947064
eMode18 <ewagandi@gmail.com>
2024-07-25 11:18:21 +0000

    coins interview question

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Change starts from Within
+
+This function calculates the fewest number of coins needed to meet a
+given total amount.
+Given a pile of coins of different values, it determines the optimal
+combination of coins.
+"""
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    min_coins = [0] + [float("inf")] * total
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+
+    return min_coins[-1] if min_coins[-1] != float("inf") else -1
